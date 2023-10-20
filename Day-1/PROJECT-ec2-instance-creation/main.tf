7b12fbcac8fe5d266e491ef05c2ac09618c01924
roopu3 <92449404+roopu3@users.noreply.github.com>
2023-10-20 15:05:45 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "ap-south-1"
 }
-
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+resource "aws_instance" "check" {
+  ami = "ami-0287a05f0ef0e9d9a"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-07c64a6e28e4ff23a"
+  key_name = "test2"
+  tags = {
+    Name = "Hello_AWS"
+  }
 }
